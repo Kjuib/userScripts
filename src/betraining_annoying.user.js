ef9fbc74ae7d690b9f8a9d34003b91dd1e533a47
hbillings <heber.billings@gmail.com>
2020-06-24 14:14:50 -0600

    Added another test

--- src/betraining_annoying.user.js
+++ src/betraining_annoying.user.js
@@ -369,8 +369,74 @@
             text: ['When a customer requests his or her private data, you must first verify their identity before disclosing it.', 'private data to his/her immediate family and close friends.'],
             answer: 'It is ok to disclose a customers private data to his/her immediate family and close friends.'
         },
+        // DR-101: Data and Records Retention
+        {
+            text: ['Retain all documents indefinitely, you wouldn’t want to throw away something important.', 'Ask a supervisor for help when you are unsure about what to retain or delete.'],
+            answer: 'Bad Idea - Retain all documents...<br />Good Idea - Ask a supervisor...<br />Bad Idea - Always maintain data...<br />Bad Idea - Destroy records...'
+        },
+        {
+            text: ['Taking accurate notes about an issue with a customer you are dealing with.', 'Text messaging a funny but crude joke to a friend from work.'],
+            answer: 'Good Idea - Taking accurate...<br />Bad Idea - Text messaging...<br />Bad Idea - Forwarding an...<br />Bad Idea - Emailing your...'
+        },
+        {
+            text: ['The minutes taken at a board meeting.', 'An email from a disgruntled customer indicating that she may file a lawsuit.'],
+            answer: 'Permanently - The minutes...<br />10 Years - An email from...<br />10 Years - Documentation...<br />Permanently - The documents...'
+        },
+        {
+            text: ['A lunch invite from a co-worker via email.', 'A policy from the Marketing Department.'],
+            answer: 'Until no... - A lunch invite...<br />10 Years - A policy from...<br />Permanently - The documents...<br />1 Year - An email from...'
+        },
+        {
+            text: ['An accident report filed by an employee.', 'A disciplinary warning for an employee who has been off task.'],
+            answer: '6 Years - An accident...<br />5 Years - A disciplinary...<br />Permanently - The termination...'
+        },
+        {
+            text: ['An accident report filed by an employee 2 years ago.', 'A document relating to the promotion of an employee 6 years ago.'],
+            answer: 'Saved - An accident...<br />Destroyed - A document...<br />Destroyed - An email regarding...'
+        },
+        {
+            text: ['You and a co-worker have been emailing back and forth all day about various subjects, unrelated to work.'],
+            answer: 'For as long as it is necessary'
+        },
+        {
+            text: ['While discussing account information with a customer, he becomes angry and threatens to file a lawsuit against us.'],
+            answer: 'Keep all documents and correspondence...'
+        },
+        {
+            text: ['You are told about a litigation hold regarding a certain customer and situation, but it is time to clean out your email'],
+            answer: 'False. A litigation hold overrides the retention schedule.'
+        },
+        {
+            text: ['Following the data retention schedule helps prevent all of the following EXCEPT'],
+            answer: 'Security breaches'
+        },
+        {
+            text: ['You have just finished negotiations and have a signed contract from a new client.'],
+            answer: '10 years'
+        },
+        {
+            text: ['Keep things simple and follow this data retention policy: Keep everything.', 'That is the best thing for our organization.'],
+            answer: 'False'
+        },
+        {
+            text: ['Which of the following statements about documents and correspondence you create and store at work is FALSE?'],
+            answer: 'They are protected by your privacy rights. Only you have access to them.'
+        },
+        {
+            text: ['While cleaning out an old closet, you discover the annual financial records from 25 years ago.'],
+            answer: 'Permanently'
+        },
+        {
+            text: ['Only create documents that are essential to conduct business.', 'Retain data for as long as required by the data retention schedule.'],
+            answer: 'If necessary, destroy data to ease the storage...'
+        },
+        {
+            text: ['All hard copy records should be crumpled up and thrown in the garbage.', 'Electronic copies of data should be placed in a folder on your computer named'],
+            answer: 'Data stored on media that cannot be erased...'
+        },
 
-        
+
+
         {
             text: ['Paper files with sensitive data are only to be kept for the immediate business need. If files are needed'],
             answer: 'True'
@@ -771,7 +837,7 @@
                 'position: absolute',
                 'top: 5px',
                 'left: 5px',
-                'width: 200px',
+                'width: 300px',
                 'background: lightblue',
                 'padding: 5px',
                 'border-radius: 5px',
